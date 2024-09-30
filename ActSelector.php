7eb1cfb57dfbf1336d7693114f56f4b78d4da562
Paul Garren <santiago.paul.garren@gmail.com>
2024-09-30 10:29:01 +0800

    style fix

--- ActSelector.php
+++ ActSelector.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MENU</title>
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </head>
@@ -16,7 +16,6 @@
 
         </div>
     </header>
-    <div>
         
         <div class="container-fluid">
             <div class="wrapper">
@@ -30,15 +29,25 @@
                     <li><a href="#t4" data-toggle="tab">Product</a></li>
                 </ul>
                 <div class="tab-content">
-                    <div id="t1" class="tab-pane fade in active">Welcome to Home</div>
-                    <div id="t2" class="tab-pane fade">Welcome to About us</div>
+                    <div id="t1" class="tab-pane active">
+                        <div class="row">
+                            <div class="col-md-3" style="background: green">activity 1</div>
+                            <div class="col-md-3">activity 1</div>
+                            <div class="col-md-3">activity 1</div>
+                            <div class="col-md-3">activity 1</div>
+                        </div>
+                    </div>
+
+                    <div id="t2" class="tab-pane fade">
+                        Welcome to About us
+                    </div>
+                        
                     <div id="t3" class="tab-pane fade">Welcome to Contact us</div>
                     <div id="t4" class="tab-pane fade">Welcome to product</div>
                 </div>
             </div>
         </div>
 
-    </div>
     <footer>
         <div class="footer">
     
